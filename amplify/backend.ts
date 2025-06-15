bd23c205052843a4496efe7226d14a190311b74d
AdelTAHANOUT <adel.tahanout@edu.igensia.com>
2025-06-15 22:40:38 +0200

    fix: remove unsupported auth config

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,3 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from '@aws-amplify/backend/auth';
 
-export const backend = defineBackend({
-  auth: auth({
-    loginWith: {
-      email: true
-    }
-  }),
-});
+export const backend = defineBackend({});
